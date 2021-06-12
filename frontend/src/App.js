8adaf714b18f9187ec988265eca4e5ca9db5ce46
BrandonKim <younjoong92@gmail.com>
2021-06-12 15:41:42 +0900

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {GetMem} from './GetMem';
+import {NodeCheck, renderedNodeInfo} from './NodeCheck';
+import {SayService} from './SayService';
+
+
+const App = () => {
+    return <div>
+        <h1>Memory Infomation</h1>
+        <GetMem />
+        <h1>Node Check Status</h1>
+        <NodeCheck />
+        <h1>Say Service</h1>
+        <SayService />
+    </div>;
+};
+
+export default App;
